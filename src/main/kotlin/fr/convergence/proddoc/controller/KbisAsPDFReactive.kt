e1d6136f8020b7dd86320eacd1c54a1273f575dd
AldoLaClasse <alain.meynier@gmail.com>
2020-09-02 17:04:12 +0200

    intégratino de code qui essaye de stocker le kbis pdf dans un indexe solr

---
+++ src/main/kotlin/fr/convergence/proddoc/controller/KbisAsPDFReactive.kt
@@ -0,0 +1,40 @@
+package fr.convergence.proddoc.controller
+
+import fr.convergence.proddoc.services.rest.client.KbisReactiveService
+import io.quarkus.vertx.web.Route;
+import io.quarkus.vertx.web.RoutingExchange;
+import io.vertx.core.http.HttpMethod;
+import javax.enterprise.context.ApplicationScoped;
+import io.vertx.core.logging.Logger
+import io.vertx.core.logging.LoggerFactory
+import kotlinx.coroutines.*
+
+@ApplicationScoped
+class KbisAsPDFReactive {
+
+    companion object {
+        private val LOG: Logger = LoggerFactory.getLogger(KbisAsPDFReactive::class.java)
+    }
+
+    var kbisSrv = KbisReactiveService()
+
+    @Route(path = "/pdfnumgestionreactive", methods = arrayOf(HttpMethod.GET))
+    fun numGestionKbis(ex :RoutingExchange)  {
+
+        val numgestion :String = ex.getParam("numgestion").orElse("empty")
+        LOG.info("Recupération paramètre numgestion : $numgestion")
+            if (!numgestion.equals("empty")) {
+                var pdfbyNumGestion : ByteArray? = null
+                runBlocking {
+                    launch {
+                        LOG.info("Appel de getPDFbyNumGestion")
+                        pdfbyNumGestion = kbisSrv.getPDFbyNumGestion(numgestion)
+                    }
+                }
+                ex.ok("$pdfbyNumGestion")
+            } else ex.ok("Numero de gestion invalide")
+            /*
+        return (xmLbyNumGestion) */
+            //@TODO retourner une response http je crois ?
+    }
+}
--- src/main/kotlin/fr/convergence/proddoc/services/rest/client/KbisReactiveService.kt
+++ src/main/kotlin/fr/convergence/proddoc/services/rest/client/KbisReactiveService.kt
@@ -17,6 +17,77 @@ class KbisReactiveService {
         private val LOG: Logger = LoggerFactory.getLogger(KbisAsXMLReactive::class.java)
     }
 
+    fun getPDFbyNumGestion(nogest: String) : ByteArray{
+        LOG.info("entree dans KbisReactiveService.getPDFbyNumGestion")
+        //Appel non-bloquant au WS myGreffe
+        val client = WebClient.create(Vertx.vertx())
+        //@TODO applicquer recommandation un client unique pour toute l'appli au lieu de l'instancier à chaque appel de la fonction
+        var pdf = client
+            .get(80, "172.31.5.20", "/convergence-greffe-web/rest/kbis/search?numGestion=$nogest")
+            .rxSend()
+            .map { it.bodyAsBuffer().bytes }
+            .blockingGet()  // je ne trouve pas comment *ne pas* utiliser blockingGet qui semblerait bloquer le thread complet
+        // il y a subscribe mais je n'arrive pas à l'utilise
+
+        // insertion du kbis PDF dans solr
+        // création d'un client solr http --> @TODO à mettre en commun quelque part
+        val urlString = "http://localhost:8983/solr/kbispdf"
+        val solr: HttpSolrClient = HttpSolrClient.Builder(urlString).build()
+
+        LOG.info("recherche si kbis pdf existe déjà pour ce dossier")
+        try {
+            val query = SolrQuery()
+            query.set("q", "kbisid:0101$nogest")
+            val response = solr.query(query)
+
+            val docList = response.results
+            when (docList.numFound.toInt()) {
+                1 -> {
+                    // si un seul document est bien trouvé on le remplace
+                    for (doc in docList) {
+                        LOG.info("insertion dans solr du kbis PDF 0101$nogest")
+                        solr.setParser(XMLResponseParser())
+                        val document = SolrInputDocument()
+                        document.addField("kbisid", "0101$nogest")
+                        document.addField("kbispdf", pdf)
+                        document.addField("version", "1")
+                        document.addField("numerolot", "1")
+                        solr.add(document)
+                        solr.commit()
+                        LOG.info("insertion dans solr terminée")
+                    }
+
+                }
+                0 -> {
+                    LOG.info("insertion dans solr du kbis PDF 0101$nogest")
+                    solr.setParser(XMLResponseParser())
+                    val document = SolrInputDocument()
+                    document.addField("kbisid", "0101$nogest")
+                    document.addField("kbispdf", pdf)
+                    document.addField("version", "1")
+                    document.addField("numerolot", "1")
+                    solr.add(document)
+                    solr.commit()
+                    LOG.info("insertion dans solr terminée")
+                }
+                else -> {
+                    // sinon on lève une erreur
+                    throw Exception("kbis 0101$nogest impossible à ajouter dan solr")
+
+                }
+            }
+        }
+        catch (e :Exception){
+            LOG.error(e.message)
+        }
+        finally {
+            LOG.info("getPDFbyNumGestion : sortie")
+            return (pdf)
+        }
+
+        //@TODO rappel kafka pour indiquer que kbis récupéré et déposé dans solr OK
+    }
+
     fun getXMLbyNumGestion(nogest: String): String {
 
         LOG.info("entree dans KbisReactiveService.getXMLbyNumGestion")
