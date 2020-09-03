97bcda8737f077c6a65c97493549c3a8f3dde479
AldoLaClasse <alain.meynier@gmail.com>
2020-09-03 16:34:48 +0200

    réparation pour que incoming sur topic "demande_kbis" fonctionne

--- src/main/kotlin/fr/convergence/proddoc/reactive/ProduitKbis.kt
+++ src/main/kotlin/fr/convergence/proddoc/reactive/ProduitKbis.kt
@@ -2,6 +2,7 @@ package fr.convergence.proddoc.reactive
 
 import fr.convergence.proddoc.libs.model.Produit
 import fr.convergence.proddoc.libs.service.ProduitCache
+import fr.convergence.proddoc.services.rest.client.KbisService
 import io.vertx.core.logging.Logger
 import io.vertx.core.logging.LoggerFactory.getLogger
 import org.eclipse.microprofile.reactive.messaging.Incoming
@@ -18,14 +19,16 @@ public class ProduitKbis(@Inject var cache: ProduitCache) {
 
     @Incoming("parametre")
     fun ecoute(produit: Produit) {
-        LOG.info("Réception du produit : $produit");
+        LOG.info("Réception du produit : $produit")
         cache.addParameter(produit)
     }
 
-    @Incoming("kbis")
+    @Incoming("produitkbis")
     fun ecouteKbis(produit: Produit) {
-        LOG.info("Réception demande Kbis : $produit");
-        //@TODO appeler le kbis avec les bon paramètres
+        LOG.info("Réception demande Kbis : $produit")
+        val kbisSrv: KbisService? = null
+        val kbisPDF = kbisSrv?.getPDFbyNumGestion(produit.valeur)
+        LOG.info("Taille du Kbis : ${kbisPDF?.size}")
     }
 
     //@TODO un outgoing sur le meme topic kbisOK ou kbisKO
